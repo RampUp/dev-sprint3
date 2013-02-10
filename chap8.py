135beaa52b1523c5b6e2766525224c69d058c61c
AJLeonardi <anthony.leonardi@gmail.com>
2013-02-10 14:56:14 -0500

    finished exercise 8.12

--- chap8.py
+++ chap8.py
@@ -1,2 +1,13 @@
 # This is where the answers to Chapter 8 questions for the BSS Dev RampUp go
-# Name:
+# Name: Anthony Leonardi
+
+def rotate_word(str, x):
+	newString = ''
+	for c in str:
+		temp=ord(c)
+		temp += x
+		temp = chr(temp)
+		newString += temp
+	return newString
+
+print rotate_word('cheer', 7)
